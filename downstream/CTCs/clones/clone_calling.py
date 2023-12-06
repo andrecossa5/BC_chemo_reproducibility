10894cb4e35a3018ed4d09977db98c00f94c937e
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-12-06 19:14:31 +0100

    back to unique GBCs

--- downstream/CTCs/clones/clone_calling.py
+++ downstream/CTCs/clones/clone_calling.py
@@ -33,8 +33,8 @@ def summary_what_we_have_longitudinal(meta, branch, dataset, print_top=True):
     PT_clones = PT_clones.loc[list(valid_clones_PT)]
     lung_clones = lung_clones.loc[list(valid_clones_lung)]
 
-    set_PT = set(PT_clones['GBC_set'].values)
-    set_lung = set(lung_clones['GBC_set'].values)
+    set_PT = set(PT_clones['GBC_set'].map(lambda x: frozenset(x.split(';'))).values)
+    set_lung = set(lung_clones['GBC_set'].map(lambda x: frozenset(x.split(';'))).values)
 
     print(
         f'''
@@ -42,7 +42,6 @@ def summary_what_we_have_longitudinal(meta, branch, dataset, print_top=True):
         n PT {len(set_PT)}, n lung {len(set_lung)}, common {len(set_PT & set_lung)}
         '''
     )
-    print([ ';'.join(x) for x in (set_PT & set_lung)])
 
     if print_top:
         print('PT')
@@ -61,19 +60,20 @@ def what_we_have_all_samples(meta):
     valid_CBC_GBC_set = []
     n_cells = {}
     set_clones = {}
+    median_n_infections = []
    
     for sample in samples:
-
         valid_CB = meta.query('sample==@sample').index.map(lambda x: x.split('_')[0])
         cells = pd.read_csv(os.path.join(path_clones, sample, 'cells_summary_table.csv'), index_col=0)
         valid_CB = set(valid_CB) & set(cells.index)
         valid_clones = cells.loc[list(valid_CB)]['clone'].unique()
         clones = pd.read_csv(os.path.join(path_clones, sample, 'clones_summary_table.csv'), index_col=0)
         clones = clones.loc[list(valid_clones)]
-        set_clones[sample] = set(clones['GBC_set'].values)
+        set_clones[sample] = set(clones['GBC_set'].map(lambda x: frozenset(x.split(';'))).values)
+        median_n_infections.extend([ len(x) for x in set_clones[sample] ])
         n_cells[sample] = len(valid_CB)
         cells_new = (
-            cells.loc[list(valid_CB)].reset_index()
+            cells.loc[valid_CB].reset_index()
             .merge(clones.reset_index(), on='clone')
             [['CBC', 'GBC_set']]
             .set_index('CBC')
@@ -90,6 +90,7 @@ def what_we_have_all_samples(meta):
     return (
         pd.DataFrame(C, index=samples, columns=samples), 
         pd.Series(n_cells), 
+        pd.Series(median_n_infections),
         pd.concat(valid_CBC_GBC_set)
     )
 
@@ -98,7 +99,7 @@ def what_we_have_all_samples(meta):
 
 
 # Args
-path_data = '/Users/IEO5505/Desktop/BC_chemo_reproducibility/data/MDA'
+path_data = '/Users/IEO5505/Desktop/BC_chemo_reproducibility/data/CTCs'
 path_clones = os.path.join(path_data, 'clonal_info')
 
 
@@ -108,54 +109,33 @@ path_clones = os.path.join(path_data, 'clonal_info')
 # Reformat meta: new metadata
 meta = pd.read_csv(os.path.join(path_data, 'cells_meta.csv'), index_col=0)
 
+# Remove shitty branch and additional, potentially unfiltered doublets
+meta = meta.query('nUMIs<=35000 and doublet_score<=.1')
+
 # Add dataset
-meta['dataset'] = (
-    meta['sample']
-    .map(lambda x: '_'.join(x.split('_')[:2]) + '_' + x.split('_')[-1] )
-)
-meta['origin'] = np.where(meta['sample'].str.contains('mets'), 'lung', 'PT')
-L = [
-    (meta['sample'].str.startswith('AC_AC')) & (meta['origin'] == 'PT'),
-    (meta['sample'].str.startswith('AC_AC')) & (meta['origin'] == 'lung'),
-    (meta['sample'].str.startswith('NT_NT')) & (meta['origin'] == 'PT'),
-    (meta['sample'].str.startswith('NT_NT')) & (meta['origin'] == 'lung'),
-    (meta['sample'].str.startswith('NT_AC')) & (meta['origin'] == 'PT'),
-    (meta['sample'].str.startswith('NT_AC')) & (meta['origin'] == 'lung')
-]
-c = [
-    'PT, treated', 'lung, double-treated', 'PT, untreated', 'lung, untreated',
-    'PT, untreated', 'lung, single-treated'
-]
-meta['condition'] = np.select(L, c)
-meta['condition'] = pd.Categorical(
-    meta['condition'], 
-    categories=[
-        'PT, treated', 'PT, untreated', 'lung, double-treated', 
-        'lung, single-treated', 'lung, untreated',
+meta['dataset'] = np.where(meta['sample'].str.contains('early'), 'ealy', 'late')
+tests = [
+    meta['sample'].str.contains('PT'),
+    meta['sample'].str.contains('CTC'),
+    meta['sample'].str.contains('lung')
     ]
-)
-
-
-##
-
+choices = ['PT', 'CTC', 'lung']
+meta['origin'] = np.select(tests, choices)
 
 
 ##
 
 
 # Checks single couples, clones
-# summary_what_we_have_longitudinal(meta, 'NT_NT', 3, print_top=True)
-
-
-##
-
 
 # Get valid CBC_GBC_sets
-common, n_cells, cbc_gbc_set = what_we_have_all_samples(meta)
+common, n_cells, n_infections, cbc_gbc_set = what_we_have_all_samples(meta)
 n_cells.sum()
 common.values.mean()
+n_infections.mean()
 
 # Rename valid GBC_sets
+cbc_gbc_set['GBC_set'] = cbc_gbc_set['GBC_set'].map(lambda x: frozenset(x.split(';')) )
 d_names = (
     cbc_gbc_set['GBC_set']
     .value_counts()
@@ -164,19 +144,44 @@ d_names = (
     ['name'].to_dict()
 )
 cbc_gbc_set['clone'] = cbc_gbc_set['GBC_set'].map(d_names)
-cbc_gbc_set['GBC_set'] = cbc_gbc_set['GBC_set'].astype('str')
+cbc_gbc_set['GBC_set'] = cbc_gbc_set['GBC_set'].map(lambda x: ';'.join(x) )
 
 # Filter meta with filtered and clonally annotated cells
 meta = cbc_gbc_set.join(meta, how='left')
 
+meta
 
-##
+meta.groupby(['sample', 'GBC_set']).size()
+
+meta.query('sample=="CTC_2_late"').groupby('clone').size()
+
+meta.query('sample=="CTC_2_late"')['GBC_set'].value_counts()
+
+meta.groupby('sample')['nUMIs'].median()
+
+meta['sample'].value_counts()
+
+
+
 
 # Save final cells
 meta.to_csv(os.path.join(path_data, 'cells_meta.csv'))
 
+
+meta[['sample', 'clone']].groupby(['sample']).value_counts()['CTC_2_late']
+
+meta.query('clone=="C0" and sample=="CTC_2_late"')
+
+meta.query('clone=="C0"').groupby('sample').size()
+
+meta.groupby('GBC_set').size().sort_values(ascending=False)
+
+
+##
+
+
 # Format QC.h5ad
-adata = sc.read(os.path.join(path_data, 'QC.h5ad'))
+adata = sc.read(os.path.join(path_data, 'QC.h5ad'))x
 adata = adata[meta.index,:].copy()
 adata.obs = meta
 
@@ -185,3 +190,4 @@ adata.write(os.path.join(path_data, 'QC.h5ad'))
 
 
 ##
+
