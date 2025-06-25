b63923e5826376547dcfb3b8f998ea150ca62e89
noemi-bulla <noemi1199@live.it>
2025-06-25 23:59:07 +0200

    pro CTC

--- downstream/CTCs/images/ctc_gsea.py
+++ downstream/CTCs/images/ctc_gsea.py
@@ -13,6 +13,7 @@ import seaborn as sns
 import pickle
 import time
 import gc
+import re
 from gseapy import prerank
 from joblib import cpu_count
 import matplotlib.pyplot as plt
@@ -27,7 +28,7 @@ path_data= os.path.join(path_main,"data", "CTCs","grn","resources")
 path_results= os.path.join(path_main, "results", "CTCs", "gene_reg_net")
 
 #Data
-#adata= sc.read_h5ad(os.path.join(path_data, "clustered.h5ad"))
+adata= sc.read_h5ad(os.path.join(path_data, "clustered.h5ad"))
 with open(os.path.join(path_data,'pro_CTC_contrasts.pickle'),'rb') as f:
     pro_ctc = pickle.load(f)
 
@@ -37,6 +38,8 @@ with open(os.path.join(path_data,'pt_ctc.pickle'), 'rb') as p:
 with open(os.path.join(path_data,'pro_CTC_all.pickle'), 'rb') as t:
     pro_ctc_all = pickle.load(t)
 
+with open(os.path.join(path_data,'pro_CTC_all_new.pickle'), 'rb') as s:
+    pro_ctc_all_new = pickle.load(s)
 ## DE ##
 
 # Prep contrast and jobs
@@ -44,6 +47,7 @@ dfs = []
 categories= ['1_proCTC_rest','2_proCTC_PT_rest','3_proCTC_PT_rest','4_proCTC_rest']
 category = ['pro_CTC_all']
 categ = ['pt_ctc','lung_ctc','pt_lung','ctc_pt/lung']
+gories = ['pro_PT_vs_CTC','pro_lung_vs_CTC','pro_vs_rest_PT','pro_vs_rest_CTC','pro_vs_rest_lung']
 
 
 for cat in categories:
@@ -64,7 +68,13 @@ for cat in categ:
     df['label'] = cat
     dfs.append(df)
 
-def fastGSEA(s, collection='MSigDB_Hallmark_2020', n_top=50):  #GO_Biological_Process_2023
+for cat in gories:
+    key = f"{cat}|genes|wilcoxon"
+    df = pro_ctc_all_new.results[key]['df'].copy()
+    df['label'] = cat
+    dfs.append(df)
+
+def fastGSEA(s, collection='MSigDB_Hallmark_2020', n_top=50):  #GO_Biological_Process_2023 #MSigDB_Hallmark_2020
     """
     Quick GSEA with gseapy.
     """
@@ -98,16 +108,17 @@ def fastGSEA(s, collection='MSigDB_Hallmark_2020', n_top=50):  #GO_Biological_Pr
 all_degs= pd.concat(dfs)
 all_degs.to_csv(os.path.join(path_data, 'CTC_DEGs_gsea.csv'))
 already_done= ['1_proCTC_rest','2_proCTC_PT_rest','pro_CTC_all','3_proCTC_PT_rest','4_proCTC_rest','pt_ctc',
-               'lung_ctc','pt_lung','ctc_pt/lung']
+               'lung_ctc','pt_lung','ctc_pt/lung', 'pro_PT_vs_CTC','pro_lung_vs_CTC',
+               'pro_vs_rest_PT','pro_vs_rest_CTC','pro_vs_rest_lung']
 for i, df in enumerate(dfs):
-    if df['label'].iloc[0] == '1_proCTC_rest':
+    if df['label'].iloc[0] == 'pro_vs_rest_lung':
         try:
             rank= df.query('comparison == "g1_vs_g0"')['effect_size'].sort_values(ascending=False)
             name_file = df['label'].iloc[0]
             df_ = fastGSEA(rank)
             df_.to_csv(
                 os.path.join(
-                    path_results, 'GSEA', 'Hallmark','ctc_pt_lung_g1_vs_g0'
+                    path_results, 'GSEA', 'Hallmark',f"{name_file}_g1_vs_g0_Hallmark"
                 )
             )
             time.sleep(2)
@@ -121,10 +132,46 @@ for i, df in enumerate(dfs):
 # Plot     
 for path_root, _, files in os.walk(os.path.join(path_results, 'GSEA','Hallmark')): #GO_biological_2023
     for x in files:
-        df = pd.read_csv(os.path.join(path_root, x), index_col=0)
-        fig, ax = plt.subplots(figsize=(8,5))
-        plu.stem_plot(df.head(20), 'NES', ax=ax)
-        plu.format_ax(ax, xlabel='NES', yticks=df.head(20).index.map(lambda x: x.split('(')[0] ), title='Top enriched pathways')
-        fig.tight_layout()
-        fig.savefig(os.path.join(path_results, 'GSEA', f'{x}_GSEA_Hallmark.png'), dpi=300)
+        if x.endswith('1_Hallmark'):
+            df = pd.read_csv(os.path.join(path_root, x), index_col=0)
+            fig, ax = plt.subplots(figsize=(8.5,5))
+            plu.stem_plot(df.head(20), 'NES', ax=ax)
+            plu.format_ax(ax, xlabel='NES', yticks=df.head(20).index.map(lambda x: x.split('(')[0] ), title='Top enriched pathways')
+            fig.tight_layout()
+            fig.savefig(os.path.join(path_results, 'GSEA', f'{x}_GSEA_Hallmark.png'), dpi=300)
+            plt.close(fig)
+
+
+## AA transporter analysis
+aa_df = pd.read_csv(os.path.join(path_data,'aa_transporters.txt')).drop(columns=['Unnamed: 1'])
+aa_list = aa_df['Transporter'].tolist()
+elchain_genes = ['ATP5F1A','UQCRC2','SDHB','NDUFB8']
+
+aa_dfs = []
+for df in dfs:
+    aa_transporter = df[df.index.isin(elchain_genes)] #aa_list 
+    aa_dfs.append(aa_transporter)
+
+for x in aa_dfs:
+    if '1_proCTC_rest' in x['label'].values:
+        df= x.query('comparison == "g1_vs_g0"')
+        print(df)
+
+def clean_name(name):
+    return re.sub(r'[\\/*?:\[\]]', '_', str(name))[:31]
+
+with pd.ExcelWriter(os.path.join(path_results,'el_chain_proCTC_DEgs.xlsx'), engine='openpyxl') as writer:  #aa_transporters_in_DEgs.xlsx
+    for df in aa_dfs:
+        label_value= df['label'].iloc[0]
+        sheet_name = clean_name(label_value)
+
+        df_fil = df[['rank', 'evidence','effect_size','comparison','label']].copy()
+        df_fil.rename(columns={'effect_size':'log2FC'}, inplace= True)
+
+        df_fil.to_excel(writer, sheet_name=sheet_name)
+
+
+
+
+
 
